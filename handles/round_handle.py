d8ec02bb5275dcf6ccc805b892e2b8dde6e3fd75
Viraj Mehta <virajm@stanford.edu>
2018-01-20 18:58:07 -0800

    adjusted heads

--- handles/round_handle.py
+++ handles/round_handle.py
@@ -6,7 +6,7 @@ class RoundHandle(Handle):
                  min_length=12e-2,
                  max_length=37.5e-2,
                  min_radius=1e-2,
-                 max_radius=3.5e-2,
+                 max_radius=3e-2,
                  constant_diameter_prob=0.7):
         super(RoundHandle, self).__init__(min_length, max_length, min_radius, max_radius)
         self.scad = 'translate([0,0, -{0}]) {{\n cylinder({0}, {1}, {2}, $fn=64);\n }};'
