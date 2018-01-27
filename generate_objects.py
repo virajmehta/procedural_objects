70c36baba29598adbef4abfd4ef2e1c57dc1f97f
Viraj Mehta <virajm@stanford.edu>
2018-01-27 15:25:40 -0800

    added convex T and L

--- generate_objects.py
+++ generate_objects.py
@@ -14,7 +14,8 @@ python generate_objects.py (1) (2) (3?)
 
 (2): the number of URDFs to generate
 
-(3)(optional): a string in ('L', 'T', 'X', 'C') which enforces a given topology of hammer
+(3)(optional): a string in ('L', 'T', 'X', 'C') which enforces a given topology 
+               of hammer
 
 Pipeline:
 1. random parts are chosen as heads and handles
@@ -101,13 +102,14 @@ def generate_hammers(directory, num_hammers, object_type=None):
     if object_type == 'C':
         heads = [ConvexHead()]
     elif object_type == 'L':
-        heads = [RoundHead(is_L=True), SquareHead(is_L=True), BreadHead(is_L=True)]
+        heads = [RoundHead(is_L=True), SquareHead(is_L=True),
+                 BreadHead(is_L=True), ConvexHead(is_L=True)]
     elif object_type == 'X':
-        heads = [RoundHead(is_X=True), SquareHead(is_X=True), BreadHead(is_X=True)]
+        heads = [RoundHead(is_X=True), SquareHead(is_X=True),
+                 BreadHead(is_X=True), ConvexHead(is_X=True)]
     else:
-        heads = [RoundHead(), SquareHead(), BreadHead()]
+        heads = [RoundHead(), SquareHead(), BreadHead(), ConvexHead()]
 
-    # heads = [ConvexHead(), RoundHead(), SquareHead(), BreadHead()]
     handles = [RoundHandle(), SquareHandle(), TriangleHandle()]
     dir_index = 0
     for i in range(num_hammers):
--- heads/convex_head.py
+++ heads/convex_head.py
@@ -12,12 +12,14 @@ class ConvexHead(Head):
                  max_radius=5e-2,
                  min_length=8e-2,
                  max_length=15e-2,
+                 is_L=False,
+                 is_X=False,
                  z_offset=0,
                  mesh_glob='/cvgl2/u/virajm/robovat_grasp/data/simulation/tools/mpi-grasp/*/*.obj'): # NOQA
         self.paths = [os.path.abspath(path) for path in glob.glob(mesh_glob)
                       if 'vhacd' in path]
         super(ConvexHead, self).__init__(max_radius, min_radius, max_length,
-                                         min_length, 0, z_offset, False, False)
+                                         min_length, 0, z_offset, is_L, is_X)
 
     def get_random_scad(self):
         return ''
@@ -37,10 +39,24 @@ class ConvexHead(Head):
             vertices *= (self.min_length / np.max(diffs))
         # make sure x is the most variant axis and z is least(hack)
         vertices = vertices[:, np.argsort(diffs)[::-1]]
+        z_diff = np.max(vertices[:, 2]) - np.min(vertices[:, 2])
+
+        # make sure it is at least as thick as it should be (too thick is OK)
+        if z_diff < self.min_radius:
+            vertices[:, 1:] *= random.uniform(self.min_radius, self.max_radius)\
+                                        * 2
 
         # make sure minimum z coordinate is -0.1
         vertices[:, 2] -= min_coordinates[np.argsort(diffs)[0]] + 0.01
 
+        if self.is_X:
+            vertices[:, 2] -= random.uniform(-15e-2, 0)
+        if self.is_L:
+            if random.choice([True, False]):
+                vertices[:, 0] -= np.min(vertices[:, 0]) + 4e-2
+            else:
+                vertices[:, 0] -= np.max(vertices[:, 0]) - 4e-2
+            vertices[:, 2] -= np.max(vertices[:, 2])
         write_mesh(fn, vertices, faces)
 
 
