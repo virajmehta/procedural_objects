410c40d1f6f3fb494d6c6b55438d3012aeeb30f4
Viraj Mehta <thebigmehtaphor@gmail.com>
2018-02-19 22:19:26 -0800

    added init

---
+++ parts/__init__.py
@@ -0,0 +1,6 @@
+from part import Part
+from round_part import RoundPart
+from convex_part import ConvexPart
+from square_part import SquarePart
+from bread_part import BreadPart
+from triangle_part import TrianglePart
