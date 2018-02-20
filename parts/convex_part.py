d868f5f3d73af27c0ed53c694fb4d41d73513005
Viraj Mehta <thebigmehtaphor@gmail.com>
2018-02-19 22:08:35 -0800

    added convex part and made it fit

--- parts/convex_part.py
+++ parts/convex_part.py
@@ -8,15 +8,17 @@ from lib import read_mesh, write_mesh
 
 class ConvexPart(Part):
     def __init__(self,
-                 min_radius=2e-2,
-                 max_radius=4e-2,
-                 min_length=12e-2,
-                 max_length=28e-2,
+                 min_length,
+                 max_length,
+                 min_width,
+                 max_width,
+                 min_depth,
+                 max_depth,
                  mesh_glob='/cvgl2/u/virajm/robovat_grasp/data/simulation/tools/mpi-grasp/*/*.obj'): # NOQA
+        super(ConvexPart, self).__init__(min_length, max_length, min_width, max_width,
+                                          min_depth, max_depth)
         self.paths = [os.path.abspath(path) for path in glob.glob(mesh_glob)
                       if 'vhacd' in path]
-        super(ConvexHandle, self).__init__(min_length, max_length, min_radius,
-                                           max_radius)
 
     def get_random_scad(self):
         return ''
@@ -32,21 +34,20 @@ class ConvexPart(Part):
         # make sure z is the most variant axis and x is least(hack)
         vertices = vertices[:, np.argsort(diffs)]
 
+        self.depth = random.uniform(self.min_depth, self.max_depth)
+        self.width = random.uniform(self.min_width, self.max_width)
+        self.length = random.uniform(self.min_length, self.max_length)
+
         # scale to make sure the length is in bounds
         z_diff = np.max(vertices[:, 2]) - np.min(vertices[:, 2])
-        vertices[:, 2] *= (random.uniform(self.min_length, self.max_length) / z_diff)
+        vertices[:, 2] *= self.depth / z_diff
 
         # make sure it is as thick as it should be 
         x_diff = np.max(vertices[:, 0]) - np.min(vertices[:, 0])
-        vertices[:, 0] *= (random.uniform(self.min_radius, self.max_radius) / x_diff)
+        vertices[:, 0] *= self.length / z_diff
         y_diff = np.max(vertices[:, 1]) - np.min(vertices[:,1])
-        vertices[:, 1] *= (random.uniform(self.min_radius, self.max_radius) / y_diff)
+        vertices[:, 1] *= self.width / z_diff
 
-
-        # make sure maximum z coordinate is 0.1
-        vertices[:, 2] -= np.max(vertices[:, 2])
-        vertices[:, 2] += 0.01
-        
         write_mesh(fn, vertices, faces)
 
 
